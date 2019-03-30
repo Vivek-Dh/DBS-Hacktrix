4e1e0560435bde10102dcbc401520b8af91453a8
US\vdehariya <VDEHARIYA@DELOITTE.COM>
2019-03-30 10:27:06 +0530

    First commit

---
+++ src/main/java/dbs/hackathon/team13/remorides/RemoridesApplication.java
@@ -0,0 +1,13 @@
+package dbs.hackathon.team13.remorides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoridesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoridesApplication.class, args);
+	}
+
+}
